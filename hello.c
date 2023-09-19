853a88e37a25cbf5794bf9b5d88b7d882866e16a
jmartinez75423 <142929714+jmartinez75423@users.noreply.github.com>
2023-09-19 13:46:17 -0500

    josh making commit changes

--- hello.c
+++ hello.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   printf("Computer Engineering\n");
   printf("Joshua Martinez\n");
   printf("Computer Science\n");
+  printf("Changes to fix it");
 
   return 0;
 }
